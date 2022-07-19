21bd887afb75d45d33f8f7c3a221ffac232d6c54
Ardian Pramudya Alphita <ardianpramudya81@gmail.com>
2022-07-19 09:15:25 +0700

    Initial Commit

---
+++ SearchList/SearchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchListApp.swift
+//  SearchList
+//
+//  Created by Ardian Pramudya Alphita on 19/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
